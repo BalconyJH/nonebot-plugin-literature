7fe53cd3f2aec3bc913c7681d0f7c3a6945a1ec3
BalconyJH <balconyjh@gmail.com>
2024-01-24 23:09:53 +0800

    :building_construction: 添加插件metadata

--- nonebot_plugin_literature/__init__.py
+++ nonebot_plugin_literature/__init__.py
@@ -1,5 +1,54 @@
-from nonebot import require
+import time
+from pathlib import Path
+
+from nonebot import require, on_command
+from nonebot.internal.adapter import Event
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters
+
+from .utils import load_xml, atom_parser
 
 require("nonebot_plugin_saa")
 require("nonebot_plugin_htmlrender")
 require("nonebot_plugin_localstore")
+
+import nonebot_plugin_saa as saa
+from nonebot_plugin_htmlrender import template_to_pic
+
+from .config import Config, plugin_config  # noqa: F401
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot_plugin_literature",
+    description="Nonebot plugin for literature.",
+    usage="""\
+    """,
+    type="application",
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_saa"),
+    config=Config,
+)
+
+literature = on_command("literature", aliases={"文献", "文献查询"}, priority=5)
+
+
+@literature.handle()
+async def _(event: Event):
+    xml = Path(__file__).parent / "response.xml"
+    data = await atom_parser(await load_xml(xml))
+    template_path = str(Path(__file__).parent / "templates")
+    template_name = "test.html.jinja"
+    start_time = time.time()
+    pic = await template_to_pic(
+        template_path=template_path,
+        template_name=template_name,
+        templates={"feed": data},
+        pages={
+            "viewport": {"width": 600, "height": 300},
+            "base_url": f"file://{template_path}",
+        },
+        wait=2,
+    )
+    end_time = time.time()
+    duration = end_time - start_time  # 计算持续时间
+
+    msg_factory = saa.MessageFactory(saa.Image(pic))
+    await literature.send(str(duration))
+    await msg_factory.finish()
--- nonebot_plugin_literature/config.py
+++ nonebot_plugin_literature/config.py
@@ -12,17 +12,17 @@ class Config(BaseModel, extra=Extra.ignore):
     timeout: int = Field(30, description="Timeout for web requests in seconds.")
     literature_render: str = Field("htmlrender", description="Render type for literature.")
 
-    @validator("literature_render")
-    def check_literature_render(cls, values):
-        if values["literature_render"] not in ["htmlrender", "PIL"]:
-            raise ValueError("literature_render must be one of 'htmlrender' or 'PIL'")
-        return values
-
     @validator("proxy")
-    def check_proxy(cls, values):
-        if not re.match(r"^http://[^\s:]+:[0-9]+$", values):
+    def check_proxy(cls, value):
+        if not re.match(r"^http://[^\s:]+:[0-9]+$", value):
             raise ValueError("proxy 必须是 http://xxx:xxx 格式")
-        return {"http": values, "https": values}
+        return {"http": value, "https": value}
+
+    @validator("literature_render")
+    def check_literature_render(cls, value):
+        if value not in ["htmlrender", "PIL"]:
+            raise ValueError("literature_render must be one of 'htmlrender' or 'PIL'")
+        return value
 
 
 global_config = get_driver().config
